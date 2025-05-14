edea75bab80ca2908ce2f47363463e987989598b
cyrildou <cyrildou@live.cn>
2025-05-14 16:38:38 +0800

    1st version

---
+++ client.go
@@ -0,0 +1,27 @@
+package go_buy365
+
+import "github.com/go-resty/resty/v2"
+
+type Client struct {
+	AccessKey string
+
+	BaseURL            string
+	WithdrawURL        string
+	WithdrawConfirmURL string
+	OrderListURL       string
+
+	ryClient *resty.Client
+}
+
+func NewClient(accessKey string, baseURL string, withdrawURL, withdrawConfirmURL, orderListURL string) *Client {
+	return &Client{
+		AccessKey: accessKey,
+
+		BaseURL:            baseURL,
+		WithdrawURL:        withdrawURL,
+		WithdrawConfirmURL: withdrawConfirmURL,
+		OrderListURL:       orderListURL,
+		
+		ryClient: resty.New(), //client实例
+	}
+}
---
+++ config.go
@@ -0,0 +1,15 @@
+package go_buy365
+
+const (
+	ACCESS_KEY = "hiahaihaihaihaihaiahiahihaih"
+	
+	//deposit
+	BASE_URL = "https://swpapi.tpex.cc/UtInRecordApi/orderGateWay"
+
+	//withdraw
+	WITHDRAW_URL         = "https://mmapi.proxima131.com/AjaxOpen/saveOutOrder"
+	WITHDRAW_CONFIRM_URL = "https://mmapi.proxima131.com/AjaxOpen/appealOutOrder"
+
+	//orderlist
+	ORDERLIST_URL = "https://mmapi.proxima131.com/AjaxOpen/getOutOrderList"
+)
